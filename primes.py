8174b14a02234c67e50aed75983f0faf0977a858
Jory AlJuhani <jory.aljuhani@kcl.ac.uk>
2022-10-04 20:53:13 +0100

    Trial 1

--- primes.py
+++ primes.py
@@ -2,5 +2,20 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    list=[]
+    counter = 0
+    primenumber = 2;
+
+    while(counter<number_of_primes):
+        primeCheck = True
+
+        for i in range(2,primenumber):
+            if(primenumber % i ==0):
+                primeCheck=False
+
+        if primeCheck:
+            list.append(primenumber)
+            counter += counter
+        primenumber+=primenumber
+
     return list
