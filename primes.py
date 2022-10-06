53197855280ce00dbd8b23cdc60e62deaae11ce0
Jory AlJuhani <jory.aljuhani@kcl.ac.uk>
2022-10-06 13:28:18 +0100

    Trial 3

--- primes.py
+++ primes.py
@@ -1,21 +1,25 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
 def primes(number_of_primes):
-    list=[]
-    counter = 0
+    list = []
+    count = 0;
     primenumber = 2;
+    if(number_of_primes<=0):
+        raise ValueError("Use a positve number greater than 0 ")
 
-    while(counter<number_of_primes):
-        primeCheck = True
+    else:
+        while (count < number_of_primes):
+            primeCheck = True
 
-        for i in range(2,primenumber):
-            if(primenumber % i ==0):
-                primeCheck=False
+            for i in range(2, primenumber):
+                if (primenumber % i == 0):
+                    primeCheck = False
 
-        if primeCheck:
-            list.append(primenumber)
-            counter += counter
-        primenumber+=primenumber
+            if primeCheck:
+                list.append(primenumber)
+                count += 1
+            primenumber += 1
 
-    return list
+
+        return list
+print(primes(0))
